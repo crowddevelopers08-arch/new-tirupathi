c96563774ba8ceba9f49c56c1e15ea606be901d4
fenil matt fenil matt <ffenilmatt@gmail.com>
2026-02-19 11:21:20 +0530

    first comsssskdd

--- components/footer.tsx
+++ components/footer.tsx
@@ -25,8 +25,8 @@ const Footer = () => {
           {/* Bottom section */}
           <div className="flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0">
             <div className="flex flex-col sm:flex-row items-center gap-4 sm:gap-6">
-              <p className="text-base max-[425px]:text-[14px] opacity-70 text-center sm:text-left">
-                Copyright © 2025 Adgro Hair Royapuram | Powered by Adglo Skin Royapuram
+             <p className="text-base max-[425px]:text-[14px] opacity-70 text-center sm:text-left">
+                Copyright © {new Date().getFullYear()} Adgro Hair Tirupati | Powered by Adgro Hair Tirupati
               </p>
             </div>
           </div>
